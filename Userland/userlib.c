ba1c5e4004bd30a13c9b83536899d51c9cd9e32d
mauritobaez <mabaez@itba.edu.ar>
2022-05-21 13:15:46 -0300

    Empezando la librería

---
+++ Userland/userlib.c
@@ -0,0 +1,37 @@
+
+#include <userlib.h>
+
+int strToNum(const char* str){
+    int i = 0;
+    int neg = 0;
+    if(str[i]=='-'){
+         neg=1;
+         i++;
+    }
+    int ans = 0;
+    for(;str[i]!='\0';i++){
+        if(str[i]<'0' || str[i]>'9'){
+            return -1;
+        }
+        ans = ans*10 + (str[i]-'0');
+    }
+    if(neg) ans*=-1;
+    return ans;
+}
+
+char strLength(const char * str){
+    int i=0;
+    while(str[i++]);
+    return i;
+}
+
+char strPrefix(const char*prefix, const char*str){
+    int i;
+    for(i=0;prefix[i] && str[i];i++){
+        if(prefix[i]!=str[i]){
+            return 0;
+        }
+    }
+    return !prefix[i];
+}
+
---
+++ Userland/userlib.h
@@ -0,0 +1,10 @@
+
+#ifndef USERLIB_H
+#define USERLIB.H
+
+
+int strToNum(const char* str);
+char strLength(const char * str);
+char strPrefix(const char*prefix, const char*str);
+
+#endif
