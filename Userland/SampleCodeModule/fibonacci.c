68aa3ce575de50282e0a78d377def69d17ca86a1
mauritobaez <mabaez@itba.edu.ar>
2022-05-28 15:38:17 -0300

    Fibonacci agregado!

---
+++ Userland/SampleCodeModule/fibonacci.c
@@ -0,0 +1,15 @@
+#include <fibonacci.h>
+
+void fibonacci(uint8_t argc, void ** argv){
+    uint64_t first=0;
+    uint64_t second=1;
+    uint64_t third;
+    //hay que hacerlo con un pequeño sleep lol
+    while(1){
+        third = first + second;
+        printWithFormat("%d",third);
+        sys_new_line(BLACK);
+        first=second;
+        second=third;
+    }
+}
---
+++ Userland/SampleCodeModule/include/fibonacci.h
@@ -0,0 +1,9 @@
+#ifndef FIBONACCI_H
+#define FIBONACCI_H
+
+#include <userlib.h>
+#include <syslib.h>
+
+void fibonacci(uint8_t argc, void ** argv);
+
+#endif
