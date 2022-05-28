1bbc6cbde5f6814b2ce3261e45e6dbd4e825cab8
RupnikF <francorupnik@gmail.com>
2022-05-28 19:19:30 -0300

    Arreglo de getcurrentdatetime

--- Kernel/time.c
+++ Kernel/time.c
@@ -2,7 +2,8 @@
 
 static unsigned long ticks = 0;
 static struct timezone_t timezone={0 , 0};
-
+static int monthdays[2][12]={{31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},{31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}};
+int isLeapYear(int year);
 void timer_handler() 
 {
 	ticks++;
@@ -22,8 +23,24 @@ void getCurrentDateTime(struct datetime_t* datetime, struct timezone_t* tzone){/
 	tzone->minutes=timezone.minutes;
 	int8_t h=getHours()+timezone.hours;
 	int8_t m=getMinutes()+timezone.minutes;
+	int8_t day=getDay();
+	int8_t month=getMonth();
+	uint16_t year=getYear()+getCentury()*100;
 	if(m<0){
-	h-=1;
+		h-=1;
+	}
+	if(h<0){
+		if(day==1){
+			month-=1;
+			if(month==0){
+				year-=1;
+				month=12;
+			}
+			day=monthdays[isLeapYear(year)][month-1]; 
+	}else{
+		day-=1;
+	}
+	
 	}
 	h%=24;
 	m%=60;
@@ -33,9 +50,7 @@ void getCurrentDateTime(struct datetime_t* datetime, struct timezone_t* tzone){/
 	datetime->segs=s;
 
 
-	uint8_t day=getDay();
-	uint8_t month=getMonth();
-	uint16_t year=getYear()+getCentury()*100;
+	
 	datetime->day=day;
 	datetime->month=month;
 	datetime->year=year;
@@ -45,10 +60,16 @@ void setTimeZone(const struct timezone_t * tzone){//Falta testing
 	timezone.hours=tzone->hours;
 	timezone.minutes=tzone->minutes;
 }
-
+int isLeapYear(int year){
+if(year%400==0){
+return 1;
+}else if(year%4==0 && year%100!=0){
+return 1;
+}
+return 0;
+}
 void sleep(uint64_t miliseconds){
     uint64_t finish = ticks + miliseconds/55 +1;
 	while(finish<initTicks){
 		initTicks=ticks;
 	}
-}
