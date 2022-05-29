434d7a052f10782cc8e43df924163b4ade2064e4
mauritobaez <mabaez@itba.edu.ar>
2022-05-28 22:40:36 -0300

    ahora sí.

--- Userland/SampleCodeModule/c/fibonacci.c
+++ Userland/SampleCodeModule/c/fibonacci.c
@@ -1,43 +1,36 @@
 #include <fibonacci.h>
 
-uint64_t printNewValue(uint64_t value1, uint64_t value2,uint8_t counter, char str[]);
-void printFirstThree(); // las hacemos aparta para que queden lindas
+uint64_t printNewValue(uint64_t value1, uint64_t value2,uint8_t counter, char str[],  char* terminations[]);
+
 
 void fibonacci(uint8_t argc, void ** argv){
     uint64_t first=FIRST;
     uint64_t second=SECOND;
     uint64_t third;
+    char* terminations[] = {"st","nd","rd","th"};
     char str[20]={0};
-    uint8_t numSerie = 3;
-    first=1;
-    printFirstThree();
+    uint8_t numSerie = 1;
     //hay que hacerlo con un pequeÃ±o sleep lol
     while(1){
-        third=printNewValue(first,second,numSerie,str);
+        third=printNewValue(first,second,numSerie,str,terminations);
         first=second;
         second=third;
         numSerie++;
-        sys_sleep(9);
+        sys_sleep(3);
     }
 }
 
-uint64_t printNewValue(uint64_t value1, uint64_t value2, uint8_t counter, char str[]){
+uint64_t printNewValue(uint64_t value1, uint64_t value2, uint8_t counter, char str[], char* terminations[]){
     uint64_t value3 = value1 + value2;
     ulongToStr(value3,str);
     printString(str);
     ulongToStr(counter,str);
     putChar(' ');
     printString(str);
-    printString("th");
+    uint8_t termination = counter%10 -1;
+    termination = (termination>=3)? 3:termination;
+    printString(terminations[termination]);
     sys_new_line(BLACK);
     return value3;
 }
 
-void printFirstThree(){
-    printString("0 1st");
-    sys_new_line(BLACK);
-    printString("1 2nd");
-    sys_new_line(BLACK);
-    printString("1 3rd");
-    sys_new_line(BLACK);
-}
